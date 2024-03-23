ebca28a9fb8998bddee5e9e4dc6a86c2323d7b38
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-24 03:29:30 +0900

    Update hello.asm

--- hello.asm
+++ hello.asm
@@ -38,6 +38,23 @@ section .data
 
 #####################################################
 
+section .text
+    global main
+
+main:
+    mov  rax, 1
+    mov  rdi, 1
+    mov  rsi, msg
+    mov  rdx, 14
+    syscall
+    mov  rax, 60
+    mov  rdi, 0
+    syscall
+    ret
+
+section .data
+    msg db "Hello, world!", 10
+
 #####################################################
 
 #####################################################
