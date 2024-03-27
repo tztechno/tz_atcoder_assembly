979dc6af08fb2773d0dbea01d9429856bc34b4c6
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-27 17:32:26 +0900

    Update ex5.asm

--- ex5.asm
+++ ex5.asm
@@ -1,3 +1,43 @@
+###################################################
+
+    [both test is ok but submission errors]
+
+###################################################
+
+section .data
+  fmt_i db "%d", 0
+  
+section .bss  
+  a resd 3 
+  b resd 3 
+
+section .text
+  extern printf, scanf
+  global main
+
+main:
+  push rbp
+  
+  lea rdi, [rel fmt_i]
+  lea rsi, [rel a]
+  call scanf wrt ..plt
+  
+  lea rdi, [rel fmt_i]
+  lea rsi, [rel b]
+  call scanf wrt ..plt
+  
+  mov rax, [rel a]
+  add rax, [rel b]
+  
+  lea rdi, [rel fmt_i]
+  mov rsi, rax
+  call printf wrt ..plt
+  
+  mov rax, 0
+  pop rbp
+  ret
+
+###################################################
 
 section .data
   fmt_i db "%d", 0
@@ -32,3 +72,5 @@ main:
   mov rax, 0
   pop rbp
   ret
+
+###################################################
