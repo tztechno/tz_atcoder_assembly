ff424c7d24d0bf2bb0818f3b9f042ec6a9844048
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-03-29 14:52:10 +0900

    Update ex5.asm

--- ex5.asm
+++ ex5.asm
@@ -1,11 +1,48 @@
 ###################################################
+　[AC version]
+
+section .data
+  fmt_i db "%d", 10, 0  ; 改行コードを追加  
+  
+section .bss  
+  a resd 3 
+  b resd 3 
+
+section .text
+  extern printf, scanf
+  global main
+
+main:
+  push rbp
+  
+  lea rdi, [rel fmt_i]
+  lea rsi, [rel a]
+  call scanf wrt ..plt
+  
+  lea rdi, [rel fmt_i]
+  lea rsi, [rel b]
+  call scanf wrt ..plt
+  
+  mov rax, [rel a]
+  add rax, [rel b]
+  
+  lea rdi, [rel fmt_i]
+  mov rsi, rax
+  call printf wrt ..plt
+  
+  mov rax, 0
+  pop rbp
+  ret
+
+
+###################################################
 
     [both test is ok but submission errors]
 
 ###################################################
 
 section .data
-  fmt_i db "%d", 0
+  fmt_i db "%d", 0      #### 改行コードが含まれていないことがWAの原因である
   
 section .bss  
   a resd 3 
@@ -41,7 +78,7 @@ main:
 
 section .data
   fmt_i db "%d", 0
-  fmt_p db "%d", 0
+  fmt_p db "%d", 0　　　　　　　　　　#### 改行コードが含まれていないことがWAの原因である
   
 section .bss  
   a resd 3 
