86de165e4beb0eafa9b8a5a167dc4d79637dedea
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-04-05 18:53:14 +0900

    Update abc107_a.asm

--- abc107_a.asm
+++ abc107_a.asm
@@ -1,4 +1,36 @@
 //abc107_a.asm
 
+section .data
+  fmt_i db "%d", 10, 0  ; 改行コードを追加  
+  
+section .bss  
+  a resd 3 
+  b resd 3 
 
+section .text
+  extern printf, scanf
+  global main
+
+main:
+  push rbp
+  
+  lea rdi, [rel fmt_i]
+  lea rsi, [rel a]
+  call scanf wrt ..plt
+  
+  lea rdi, [rel fmt_i]
+  lea rsi, [rel b]
+  call scanf wrt ..plt
+  
+  mov rax, [rel a] ; rax = a
+  sub rax, [rel b] ; rax -b
+  add rax, 1 ; rax += 1
+
+  lea rdi, [rel fmt_i]
+  mov rsi, rax
+  call printf wrt ..plt
+  
+  mov rax, 0
+  pop rbp
+  ret
 
